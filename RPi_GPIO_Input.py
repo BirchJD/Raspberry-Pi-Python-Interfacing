495bf50c9955387ade2756e0770f4cdcff7d8c87
BirchJD <jason-birch@hotmail.co.uk>
2016-07-31 16:33:03 +0100

    Added Raspberry Pi Simple Input Python source code.

---
+++ RPi_GPIO_Input.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import sys
+import time
+import datetime
+import RPi.GPIO
+
+
+#/****************************/
+#/* USER DEFINABLE CONSTANTS */
+#/****************************/
+GPIO_INPUT_PIN = 14
+
+
+#  /*******************************************/
+# /* Configure Raspberry Pi GPIO interfaces. */
+#/*******************************************/
+def InitGPIO():
+   RPi.GPIO.setmode(RPi.GPIO.BCM)
+   RPi.GPIO.setup(GPIO_INPUT_PIN, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
+   RPi.GPIO.add_event_detect(GPIO_INPUT_PIN, RPi.GPIO.RISING, callback=InputCallback, bouncetime=200)
+#   RPi.GPIO.add_event_detect(GPIO_INPUT_PIN, RPi.GPIO.FALLING, callback=InputCallback, bouncetime=200)
+
+
+#  /**************************************/
+# /* Raspberry Pi GPIO interupt routine. */
+#/**************************************/
+def InputCallback(GpioPin):
+   Now = datetime.datetime.now()
+   print(Now.strftime("%Y-%m-%d %H:%M:%S") + " - GPIO: " + format(GpioPin, "00d"))
+
+
+
+#  /**************************/
+# /* Main application loop. */
+#/**************************/
+InitGPIO()
+Key = 0
+while Key == 0:
+   time.sleep(1)
+   Key = sys.stdin.read(1)
+
+
+#  /*************************************************/
+# /* Close Raspberry Pi GPIO use before finishing. */
+#/*************************************************/
+RPi.GPIO.cleanup()
+
